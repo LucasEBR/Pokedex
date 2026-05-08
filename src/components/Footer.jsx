23f2584faac88d73f7c646ff908e496e16643b87
Laboratorio Unidade Fatima <labunivas@univas.edu.br>
2026-05-07 21:46:44 -0300

    feat: estrutura inicial da pokedex

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p>Desenvolvido por [seu amigo e seu irmão (lucas e)] - Programação Frontend - UNIVAS</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,14 @@
+function PokemonCard() {
+  return ( 
+    <div>
+      <img 
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/418.png"
+        alt="Imagem do Pokémon"
+      />
+      <h2>#025 - Buizel</h2>
+      <p>Tipo: Água</p>
+    </div>
+  )
+}
+
+export default PokemonCard
