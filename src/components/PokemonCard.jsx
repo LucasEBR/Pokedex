f23c20ac6f2400761718e47f5d5d8bfc174a4a7d
Laboratorio Unidade Fatima <labunivas@univas.edu.br>
2026-05-13 20:12:50 -0300

    Commit-13-05

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,14 +1,13 @@
-function PokemonCard() {
-  return ( 
+function PokemonCard({ id, name, type, imageUrl }) {
+  return (
     <div>
-      <img 
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/418.png"
-        alt="Imagem do Pokémon"
-      />
-      <h2>#025 - Buizel</h2>
-      <p>Tipo: Água</p>
+      <img src={imageUrl} alt={name} />
+      <h2>
+        #{String(id).padStart(3, '0')} — {name}
+      </h2>
+      <p>Tipo: {type}</p>
     </div>
-  )
+  );
 }
 
 export default PokemonCard
